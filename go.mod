82825419ce9231d34272cb20416025989ed5192b
daijunkai <daijunkai@ishumei.com>
2024-10-17 18:59:14 +0800

    monitor-prometheus

--- go.mod
+++ go.mod
@@ -5,8 +5,9 @@ go 1.19
 require (
 	github.com/Masterminds/sprig/v3 v3.2.2
 	github.com/bytedance/sonic v1.11.3
-	github.com/cloudwego/kitex v0.4.3
+	github.com/cloudwego/kitex v0.4.4
 	github.com/mitchellh/mapstructure v1.5.0
+	github.com/prometheus/client_golang v1.12.2
 	github.com/samber/lo v1.39.0
 	github.com/spf13/viper v1.14.0
 	github.com/stretchr/testify v1.9.0
@@ -15,21 +16,28 @@ require (
 require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/huandu/xstrings v1.3.1 // indirect
 	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/shopspring/decimal v1.2.0 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
@@ -42,6 +50,8 @@ require (
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
 	golang.org/x/text v0.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20221024183307-1bc688fe9f3e // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ monitor-prometheus/export.go
@@ -0,0 +1,82 @@
+package prometheus
+
+import (
+	"strconv"
+	"time"
+
+	prom "github.com/prometheus/client_golang/prometheus"
+)
+
+type ArchClientLabel struct {
+	Caller string
+	Callee string
+	Method string
+	Status string
+	Detail string
+	Retry  int
+}
+
+func ArchClientThroughput(labels *ArchClientLabel) {
+	if labels == nil {
+		return
+	}
+	var l = make(prom.Labels)
+	l[labelKeyCaller] = defaultValIfEmpty(labels.Caller, unknownLabelValue)
+	l[labelKeyCallee] = defaultValIfEmpty(labels.Callee, unknownLabelValue)
+	l[labelKeyMethod] = defaultValIfEmpty(labels.Method, unknownLabelValue)
+	l[labelKeyStatus] = defaultValIfEmpty(labels.Status, unknownLabelValue)
+	l[labelKeyDetail] = removeDynamicDetail(labels.Detail, unknownLabelValue)
+	l[labelKeyRetry] = defaultValIfEmpty(strconv.Itoa(labels.Retry), unknownLabelValue)
+	_ = counterAdd(globalClientHandledCounter, 1, l)
+}
+
+func ArchClientLatencyUs(value time.Duration, labels *ArchClientLabel) {
+	if labels == nil {
+		return
+	}
+	var l = make(prom.Labels)
+	l[labelKeyCaller] = defaultValIfEmpty(labels.Caller, unknownLabelValue)
+	l[labelKeyCallee] = defaultValIfEmpty(labels.Callee, unknownLabelValue)
+	l[labelKeyMethod] = defaultValIfEmpty(labels.Method, unknownLabelValue)
+	l[labelKeyStatus] = defaultValIfEmpty(labels.Status, unknownLabelValue)
+	l[labelKeyDetail] = removeDynamicDetail(labels.Detail, unknownLabelValue)
+	l[labelKeyRetry] = defaultValIfEmpty(strconv.Itoa(labels.Retry), unknownLabelValue)
+	_ = histogramObserve(globalClientHandledHistogram, value, l)
+}
+
+type ArchServerLabel struct {
+	Caller string
+	Callee string
+	Method string
+	Status string
+	Detail string
+	Retry  int
+}
+
+func ArchServerThroughput(labels *ArchServerLabel) {
+	if labels == nil {
+		return
+	}
+	var l = make(prom.Labels)
+	l[labelKeyCaller] = defaultValIfEmpty(labels.Caller, unknownLabelValue)
+	l[labelKeyCallee] = defaultValIfEmpty(labels.Callee, unknownLabelValue)
+	l[labelKeyMethod] = defaultValIfEmpty(labels.Method, unknownLabelValue)
+	l[labelKeyStatus] = defaultValIfEmpty(labels.Status, unknownLabelValue)
+	l[labelKeyDetail] = removeDynamicDetail(labels.Detail, unknownLabelValue)
+	l[labelKeyRetry] = defaultValIfEmpty(strconv.Itoa(labels.Retry), unknownLabelValue)
+	_ = counterAdd(globalServerHandledCounter, 1, l)
+}
+
+func ArchServerLatencyUs(value time.Duration, labels *ArchServerLabel) {
+	if labels == nil {
+		return
+	}
+	var l = make(prom.Labels)
+	l[labelKeyCaller] = defaultValIfEmpty(labels.Caller, unknownLabelValue)
+	l[labelKeyCallee] = defaultValIfEmpty(labels.Callee, unknownLabelValue)
+	l[labelKeyMethod] = defaultValIfEmpty(labels.Method, unknownLabelValue)
+	l[labelKeyStatus] = defaultValIfEmpty(labels.Status, unknownLabelValue)
+	l[labelKeyDetail] = removeDynamicDetail(labels.Detail, unknownLabelValue)
+	l[labelKeyRetry] = defaultValIfEmpty(strconv.Itoa(labels.Retry), unknownLabelValue)
+	_ = histogramObserve(globalServerHandledHistogram, value, l)
+}
